276c4ff729a8efee6dcc3a6a4f2f633386bbca44
MURAT ÇOBAN <murcoban@gmail.com>
2021-01-22 21:46:13 +0300

    SAD

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
